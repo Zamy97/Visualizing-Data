9c457ccf0cf66c71eb6e7b24d54c5d44db71647d
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-22 00:39:17 -0800

    can see visually how two dice woule look like

--- die_visual.py
+++ die_visual.py
@@ -4,11 +4,11 @@ from die import Die
 
 # Create a D6
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # Make some rolls, and store results in a list
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -23,10 +23,11 @@ for value in range(2, max_result+1):
 hist = pygal.Bar()
 
 hist.title = "Results of rolling two D6 1000 times."
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
+       '13', '14', '15', '16']
 hist.x_title = "Result"
 hist.y_title = "Frequency of result"
 
-hist.add('D6 + D6', frequencies)
+hist.add('D6 + D10', frequencies)
 hist.render_to_file('die_visual.svg')
 print(frequencies)
